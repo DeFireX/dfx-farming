ba6eaa9b091fe8451ef60f75cf13892b73d81199
Dmitx <37021407+Dmitx@users.noreply.github.com>
2021-03-05 18:33:07 +0300

    Add test coverage for DfxFarmingPool contract
    Add emergencyWithdraw tests
    Add deposit/withdraw LPs and rewards tests
    Add updatePool and massUpdatePools tests
    Add pendingDfx tests

--- test/farming/DfxFarmingPool.test.js
+++ test/farming/DfxFarmingPool.test.js
@@ -29,7 +29,7 @@ describe('Contract farming/DfxFarmingPool.sol', function () {
     const TOKEN_SYMBOL = 'TEST';
     const INITIAL_SUPPLY = ether('1000');
 
-    const REWARD_TOKENS_SUPPLY = ether('1000');
+    const REWARD_TOKENS_SUPPLY = ether('5');
     const REWARD_TOKENS_PER_BLOCK = ether('1');
 
     const ALLOC_POINT_LP_1 = ether('0.6');
@@ -194,5 +194,320 @@ describe('Contract farming/DfxFarmingPool.sol', function () {
             );
         });
     });
+
+    describe('emergencyWithdraw function', function () {
+        it('should allow emergency withdraw', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
     
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+            (await this.tokenLp1.balanceOf(this.farmingPool.address))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            await this.farmingPool.emergencyWithdraw(
+                new BN(0),
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+            (await this.tokenLp1.balanceOf(this.farmingPool.address))
+                .should.be.bignumber.equal(ether('0'));
+        });
+    });
+
+    describe('deposit/withdraw LPs and rewards for user and devaddr', function () {
+        it('should be correct for first pool', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+    
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            // 4 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+    
+            // LP pool reward: 1 tokens * 4 blocks * 60% = 2.40 tokens
+            // for user: reward * 90% = 2.16 tokens
+            // for devaddr: reward * 10% = 0.24 tokens
+            (await this.tokenRewards.balanceOf(user))
+                .should.be.bignumber.equal(ether('2.16'));
+            (await this.tokenRewards.balanceOf(devaddr))
+                .should.be.bignumber.equal(ether('0.24'));
+            (await this.tokenRewards.balanceOf(this.reservoir.address))
+                .should.be.bignumber.equal(REWARD_TOKENS_SUPPLY.sub(ether('2.4')));
+        });
+
+        it('should be correct for second pool', async function () {
+            await this.tokenLp2.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+    
+            await this.farmingPool.deposit(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            // 4 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+    
+            // LP pool reward: 1 tokens * 4 blocks * 40% = 1.60 tokens
+            // for user: reward * 90% = 1.44 tokens
+            // for devaddr: reward * 10% = 0.16 tokens
+            (await this.tokenRewards.balanceOf(user))
+                .should.be.bignumber.equal(ether('1.44'));
+            (await this.tokenRewards.balanceOf(devaddr))
+                .should.be.bignumber.equal(ether('0.16'));
+            (await this.tokenRewards.balanceOf(this.reservoir.address))
+                .should.be.bignumber.equal(REWARD_TOKENS_SUPPLY.sub(ether('1.6')));
+        });
+
+        it('should be correct for both pools', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+            await this.tokenLp2.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+            
+            // next block
+            await this.farmingPool.deposit(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            // 5 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            // 5 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY); 
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+    
+            // LP pools reward: 1 tokens * 5 blocks = 5 tokens
+            // for user: reward * 90% = 4.5 tokens
+            // for devaddr: reward * 10% = 0.5 tokens
+            (await this.tokenRewards.balanceOf(user))
+                .should.be.bignumber.equal(ether('4.5'));
+            (await this.tokenRewards.balanceOf(devaddr))
+                .should.be.bignumber.equal(ether('0.5'));
+            (await this.tokenRewards.balanceOf(this.reservoir.address))
+                .should.be.bignumber.equal(REWARD_TOKENS_SUPPLY.sub(ether('5')));
+        });
+
+        it('should be correct after reaching the Reservoir balance', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+            await this.tokenLp2.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+            
+            // next block
+            await this.farmingPool.deposit(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 4 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            // 6 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            // 6 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(1),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY); 
+            (await this.tokenLp2.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+
+            // LP pools reward: 1 tokens * 6 blocks = 6 tokens
+            // Reservoir balance = 5 tokens (max)
+            // for user: reward * 90% = 4.5 tokens
+            // for devaddr: reward * 10% = 0.5 tokens
+            (await this.tokenRewards.balanceOf(user))
+                .should.be.bignumber.equal(ether('4.5'));
+            (await this.tokenRewards.balanceOf(devaddr))
+                .should.be.bignumber.equal(ether('0.5'));
+            (await this.tokenRewards.balanceOf(this.reservoir.address))
+                .should.be.bignumber.equal(REWARD_TOKENS_SUPPLY.sub(ether('5')));
+        });
+    });
+
+    describe('updatePool and massUpdatePools functions', function () {
+        it('should call updatePool after deposit', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+    
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            await this.farmingPool.updatePool(new BN(0), { from: anyone });
+        });
+
+        it('should call massUpdatePools after deposit', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+    
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            await this.farmingPool.massUpdatePools({ from: anyone });
+        });
+    });
+
+    describe('pendingDfx function', function () {
+        it('should be equal to claimed tokens', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+    
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 3 blocks
+            await time.advanceBlock();
+            await time.advanceBlock();
+            await time.advanceBlock();
+    
+            // 3 blocks after start
+            let pendingTokens = await this.farmingPool.pendingDfx(new BN(0), user);
+    
+            // 4 blocks after start (next block)
+            await this.farmingPool.withdraw(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+    
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(INITIAL_SUPPLY);
+    
+            // pendingTokens after 3 blocks, real reward after 4 blocks
+            (await this.tokenRewards.balanceOf(user))
+                .should.be.bignumber.equal(pendingTokens.mul(new BN(4)).div(new BN(3)));
+        });
+
+        it('should not be more than the Reservoir balance', async function () {
+            await this.tokenLp1.approve(this.farmingPool.address, INITIAL_SUPPLY, { from: user });
+
+            await this.farmingPool.deposit(
+                new BN(0),
+                INITIAL_SUPPLY,
+                { from: user });
+
+            (await this.tokenLp1.balanceOf(user))
+                .should.be.bignumber.equal(ether('0'));
+
+            // skip 10 blocks
+            await time.advanceBlockTo((await time.latestBlock()).add(new BN(10)));
+            
+            // LP pools reward: 1 tokens * 10 blocks * 60% = 6 tokens
+            // Reservoir balance = 5 tokens (max)
+            // only 5 tokens can be distributed (4.5 tokens for user)
+            let maxReward = REWARD_TOKENS_SUPPLY.mul(new BN(90)).div(new BN(100));
+
+            (await this.farmingPool.pendingDfx(new BN(0), user))
+                .should.be.bignumber.equal(maxReward);
+
+            // skip 1 block
+            await time.advanceBlock();
+
+            (await this.farmingPool.pendingDfx(new BN(0), user))
+                .should.be.bignumber.equal(maxReward);
+        });
+    });
 });
