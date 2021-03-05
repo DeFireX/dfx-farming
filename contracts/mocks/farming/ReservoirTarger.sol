9cf4018f84ec7b45c1b3846f886bfd3abf020e95
Dmitx <37021407+Dmitx@users.noreply.github.com>
2021-03-05 16:41:42 +0300

    Add mocks contracts for testing

---
+++ contracts/mocks/farming/ReservoirTarger.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "../../farming/interfaces/IReservoir.sol";
+
+contract ReservoirTarget {
+    IReservoir public reservoir;
+
+    function setReservoir(IReservoir _reservoir) public {
+        reservoir = _reservoir;
+    }
+
+    function runDrip(uint256 _amount) public {
+        reservoir.drip(_amount);
+    }
+}
---
+++ contracts/mocks/token/ERC20Mock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// mock ERC20 token
+contract ERC20Mock is ERC20 {
+    constructor (
+        string memory name,
+        string memory symbol,
+        address initialAccount,
+        uint256 initialBalance
+    ) public payable ERC20(name, symbol) {
+        _mint(initialAccount, initialBalance);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+}
