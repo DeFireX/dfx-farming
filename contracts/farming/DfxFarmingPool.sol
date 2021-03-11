346e9bfff89addb21cd620628bddb2d1b74d4071
Dmitx <37021407+Dmitx@users.noreply.github.com>
2021-03-11 23:23:49 +0300

    Update getMultiplier to pure

--- contracts/farming/DfxFarmingPool.sol
+++ contracts/farming/DfxFarmingPool.sol
@@ -148,7 +148,7 @@ contract DfxFarmingPool is Ownable {
     }
 
     // Return reward multiplier over the given _from to _to block.
-    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+    function getMultiplier(uint256 _from, uint256 _to) public pure returns (uint256) {
         return _to.sub(_from);
     }
 
